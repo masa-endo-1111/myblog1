f3e6ae7fe86e1f8f8c5ea17d40d1f24fffc3d446
masa-endo-1111 <madja1111@icloud.com>
2020-09-20 04:07:15 +0900

    second_commit
    ActiveAdminの編集フォームにエディター導入

--- app/admin/posts.rb
+++ app/admin/posts.rb
@@ -1,4 +1,19 @@
 ActiveAdmin.register Post do
+  permit_params :title, :content, :auther, :date
+
+  show do
+    render partial: 'resources/show'
+  end
+
+  form do |f|
+    f.inputs 'Article' do
+      f.input :title
+      f.input :content, as: :froala_editor
+      f.input :auther
+      f.input :date
+    end
+    f.actions
+  end
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
