a8312d8850dbd938f89be77bf28e6625ac610b13
masa-endo-1111 <madja1111@icloud.com>
2020-09-22 21:46:25 +0900

    first_commit
    ブックマーク機能実装のため、bookmarkモデル作成後マイグレーションファイル編集し、マイグレート。
    アソシエーションを設定。

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
+  has_many :bookmarks
+  has_many :users, through: :bookmarks
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :comments, dependent: :destroy
+  has_many :bookmarks, dependent: :destroy
+  has_many :bookmark_posts, through: :bookmarks, source: :post
 end
