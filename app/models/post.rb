82126abf33e10066321249ae82896591f6129d42
masa-endo-1111 <madja1111@icloud.com>
2020-09-24 07:10:50 +0900

    seccond_commit
    ブックマークされた記事が外部キー制約の影響で削除できなかったので、post.rbにdependent: :destroy追加。動作確認問題なし。

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
-  has_many :bookmarks
+  has_many :bookmarks, dependent: :destroy
   has_many :users, through: :bookmarks
 
   def bookmarked_by?(user)
